789fdebcc8729b461eaae236b551531ac6fa534d
Joshua Hagedorn <josh.hagedorn@gmail.com>
2016-02-17 18:49:27 -0500

    ... and the quotes...

--- global.R
+++ global.R
@@ -20,7 +20,7 @@
   due <- as.Date("2017/9/30")
 
 # Import total # needing assessment per CMHSP from local script
-  source(totals.R)
+  source("totals.R")
     
 # Load de-identified data
   scrub_sis <- read.csv("data/scrub_sis.csv")
