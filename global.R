e55a71df32a2ef3fb56d6dbd154b86e0088b31c1
Joshua Hagedorn <josh.hagedorn@gmail.com>
2016-02-17 18:37:35 -0500

    Make global global

--- global.R
+++ global.R
@@ -1,19 +1,5 @@
 ## global.R ##
 
-# LRP-specific code
-
-# Load de-identified data
-  scrub_sis <- read.csv("data/scrub_sis.csv")
-
-# Make totals df
-  totals <- data.frame(c("All", levels(scrub_sis$agency)))
-  names(totals)[1] <- "agency"
-  totals$total <- c(3826,372,740,1884,592,238) # manually assign totals to CMHs
-
-##############################################################################
-
-# Shared code
-
 # Load fun, data, libs, source files
   library(shinydashboard)
   library(shinythemes)
